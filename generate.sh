e8bfc605cec26ceaad01ec366b587c808a06dbce
Shubham <10323297+shubham-vunet@users.noreply.github.com>
2026-03-30 18:21:51 +0530

    Add Angular

--- generate.sh
+++ generate.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
-set -e
+set -ex
 export CI=true
 export NX_DAEMON=false
 export NX_INTERACTIVE=false
 export NX_IGNORE_UNSUPPORTED_TS_SETUP="true"
 
-[ ! -d "apps/react-app" ] && (echo "Generating React App..."; npx nx g @nx/react:app --directory=apps/react-app --e2eTestRunner=none --bundler=vite --style=css --interactive=false) || echo "React App exists"
+if [ ! -d "apps/react-app" ]; then
+    echo "Generating React App..."
+    npx nx g @nx/react:app --directory=apps/react-app --e2eTestRunner=none --bundler=vite --style=css --interactive=false
+else
+    echo "React App exists"
+fi
 
-[ ! -d "apps/vue-app" ] && (echo "Generating Vue App..."; npx nx g @nx/vue:app --directory=apps/vue-app --e2eTestRunner=none --style=css --interactive=false) || echo "Vue App exists"
+if [ ! -d "apps/vue-app" ]; then
+    echo "Generating Vue App..."
+    npx nx g @nx/vue:app --directory=apps/vue-app --e2eTestRunner=none --style=css --interactive=false
+else
+    echo "Vue App exists"
+fi
 
-[ ! -d "apps/angular-app" ] && (echo "Generating Angular App..."; npx nx g @nx/angular:app --directory=apps/angular-app --e2eTestRunner=none --style=css --interactive=false) || echo "Angular App exists"
+if [ ! -d "apps/angular-app" ]; then
+    echo "Generating Angular App..."
+    npx nx g @nx/angular:app --directory=apps/angular-app --e2eTestRunner=none --style=css --interactive=false
+else
+    echo "Angular App exists"
+fi
 
-[ ! -d "apps/next-app" ] && (echo "Generating Next App..."; npx nx g @nx/next:app --directory=apps/next-app --e2eTestRunner=none --style=css --interactive=false) || echo "Next App exists"
+if [ ! -d "apps/next-app" ]; then
+    echo "Generating Next App..."
+    npx nx g @nx/next:app --directory=apps/next-app --e2eTestRunner=none --style=css --interactive=false
+else
+    echo "Next App exists"
+fi
 
-[ ! -d "apps/nuxt-app" ] && (echo "Generating Nuxt App..."; npx nx g @nx/nuxt:app --directory=apps/nuxt-app --e2eTestRunner=none --style=css --interactive=false) || echo "Nuxt App exists"
+if [ ! -d "apps/nuxt-app" ]; then
+    echo "Generating Nuxt App..."
+    npx nx g @nx/nuxt:app --directory=apps/nuxt-app --e2eTestRunner=none --style=css --interactive=false
+else
+    echo "Nuxt App exists"
+fi
