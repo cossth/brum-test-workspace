06af27a7db0ae55c4aead0caf077bc5fd1dd63f1
Shubham <10323297+shubham-vunet@users.noreply.github.com>
2026-03-30 18:07:38 +0530

    fix(nx): remove positional arguments and appDir flag to fix schema generation errors

--- generate.sh
+++ generate.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -e
+export CI=true
+export NX_DAEMON=false
+export NX_INTERACTIVE=false
 
 echo "Generating React App..."
-npx nx g @nx/react:app react-app --directory=apps/react-app --e2eTestRunner=none --bundler=vite --style=css --interactive=false
+npx nx g @nx/react:app --directory=apps/react-app --e2eTestRunner=none --bundler=vite --style=css --interactive=false --projectNameAndRootFormat=as-provided
 
 echo "Generating Vue App..."
-npx nx g @nx/vue:app vue-app --directory=apps/vue-app --e2eTestRunner=none --style=css --interactive=false
+npx nx g @nx/vue:app --directory=apps/vue-app --e2eTestRunner=none --style=css --interactive=false --projectNameAndRootFormat=as-provided
 
 echo "Generating Angular App..."
-npx nx g @nx/angular:app angular-app --directory=apps/angular-app --e2eTestRunner=none --style=css --interactive=false
+npx nx g @nx/angular:app --directory=apps/angular-app --e2eTestRunner=none --style=css --interactive=false --projectNameAndRootFormat=as-provided
 
 echo "Generating Next App..."
-npx nx g @nx/next:app next-app --directory=apps/next-app --e2eTestRunner=none --style=css --appDir=true --interactive=false
+npx nx g @nx/next:app --directory=apps/next-app --e2eTestRunner=none --style=css --interactive=false --projectNameAndRootFormat=as-provided
 
 echo "Generating Nuxt App..."
-npx nx g @nx/nuxt:app nuxt-app --directory=apps/nuxt-app --e2eTestRunner=none --style=css --interactive=false
+npx nx g @nx/nuxt:app --directory=apps/nuxt-app --e2eTestRunner=none --style=css --interactive=false --projectNameAndRootFormat=as-provided
