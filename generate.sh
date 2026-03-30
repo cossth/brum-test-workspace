3d16aa30bdc595bb85f40ed563de663a1be388d8
Shubham <10323297+shubham-vunet@users.noreply.github.com>
2026-03-30 18:18:51 +0530

    Add react

--- generate.sh
+++ generate.sh
@@ -5,16 +5,16 @@ export NX_DAEMON=false
 export NX_INTERACTIVE=false
 
 echo "Generating React App..."
-npx nx g @nx/react:app --directory=apps/react-app --e2eTestRunner=none --bundler=vite --style=css --interactive=false --projectNameAndRootFormat=as-provided
+npx nx g @nx/react:app --directory=apps/react-app --e2eTestRunner=none --bundler=vite --style=css --interactive=false
 
 echo "Generating Vue App..."
-npx nx g @nx/vue:app --directory=apps/vue-app --e2eTestRunner=none --style=css --interactive=false --projectNameAndRootFormat=as-provided
+npx nx g @nx/vue:app --directory=apps/vue-app --e2eTestRunner=none --style=css --interactive=false
 
 echo "Generating Angular App..."
-npx nx g @nx/angular:app --directory=apps/angular-app --e2eTestRunner=none --style=css --interactive=false --projectNameAndRootFormat=as-provided
+npx nx g @nx/angular:app --directory=apps/angular-app --e2eTestRunner=none --style=css --interactive=false
 
 echo "Generating Next App..."
-npx nx g @nx/next:app --directory=apps/next-app --e2eTestRunner=none --style=css --interactive=false --projectNameAndRootFormat=as-provided
+npx nx g @nx/next:app --directory=apps/next-app --e2eTestRunner=none --style=css --interactive=false
 
 echo "Generating Nuxt App..."
-npx nx g @nx/nuxt:app --directory=apps/nuxt-app --e2eTestRunner=none --style=css --interactive=false --projectNameAndRootFormat=as-provided
+npx nx g @nx/nuxt:app --directory=apps/nuxt-app --e2eTestRunner=none --style=css --interactive=false
